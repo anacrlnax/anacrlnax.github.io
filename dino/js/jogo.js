defc439008d1432724eff5164d3642a8edfaa68c
ana carolina <ana_carolina-martins@estudante.sc.senai.br>
2020-11-03 17:54:41 -0200

    Pulo do dino

--- dino/js/jogo.js
+++ dino/js/jogo.js
@@ -1,2 +1,44 @@
 //jogo.js
 console.log('Início de jogo!');
+
+const dino = document.querySelector('.dino');
+//console.log(dino);
+let dinoPosition = 0;
+let estaPulando = false;
+
+//Verificar se a tecla espaço foi pressionada
+document.addEventListener('keydown',(event)=>{
+    if (event.code === 'Space')
+        if(!estaPulando) pular();
+});
+
+function pular(){
+    let intervaloPulo = setInterval(()=>{
+        estaPulando = true;
+        if (dinoPosition >= 250){
+            console.log('topo!');
+            clearInterval(intervaloPulo);
+            let intervaloQueda = setInterval(()=>{
+                if(dinoPosition <= 0) {
+                    
+                    console.log('Chão');
+                    estaPulando = false;
+                    clearInterval(intervaloQueda);
+                    
+                } else {
+                    //Decrementar a posição do Dino
+                    dinoPosition -= 20;
+                    //Atualizar a posição na tela
+                    dino.style.bottom = dinoPosition + 'px';
+                }
+            },20);
+
+        } else {
+   //Incrementar a posição
+    dinoPosition += 20;
+    //Atualizar a posição na tela 
+    dino.style .bottom = dinoPosition + 'px';
+        }
+    
+    },20);
+};
